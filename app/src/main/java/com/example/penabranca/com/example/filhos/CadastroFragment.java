3057b830e33e1379e296f255ddf10aa6cb3c2964
teba01 <matheussc8171@gmail.com>
2019-11-21 19:08:08 -0300

    Database Funcionando

--- app/src/main/java/com/example/penabranca/com/example/filhos/CadastroFragment.java
+++ app/src/main/java/com/example/penabranca/com/example/filhos/CadastroFragment.java
@@ -1,6 +1,7 @@
 package com.example.penabranca.com.example.filhos;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -15,19 +16,26 @@ import android.widget.Switch;
 import android.widget.Toast;
 
 import com.example.penabranca.R;
+import com.example.penabranca.com.example.consulente.NavigacaoCActivity;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 
 public class CadastroFragment extends Fragment {
 
     EditText nome, senha, email, telefone;
-    Button botaocadastrar;
+    Button botaocadastrar,botaovoltar;
     Switch swtAdm;
     View vista;
+    FirebaseDatabase database;
+    DatabaseReference ref;
     private FirebaseAuth mFirebaseAuth;
+    private Usuario usuario;
+    private Admnistrador admnistrador;
 
     public CadastroFragment() {
         // Required empty public constructor
@@ -40,6 +48,7 @@ public class CadastroFragment extends Fragment {
         // Inflate the layout for this fragment
         vista = inflater.inflate(R.layout.fragment_cadastro, container, false);
 
+        botaovoltar = vista.findViewById(R.id.botaovoltar);
         botaocadastrar = vista.findViewById(R.id.botaocadastrar);
         nome = vista.findViewById(R.id.nome);
         senha = vista.findViewById(R.id.senha);
@@ -47,29 +56,62 @@ public class CadastroFragment extends Fragment {
         telefone = vista.findViewById(R.id.telefone);
         swtAdm = vista.findViewById(R.id.swtAdm);
         mFirebaseAuth = FirebaseAuth.getInstance();
+
+        usuario = new Usuario();
+        database = FirebaseDatabase.getInstance();
+        ref = database.getReference().child("Usuario");
+
 //
+        //botaocadastrar.setOnClickListener(new View.OnClickListener() {
+            //@Override
+           // public void onClick(View v) {
+               // mFirebaseAuth.createUserWithEmailAndPassword(email.getText().toString(), senha.getText().toString())
+                      //  .addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
+                      //      @Override
+        // public void onComplete(@NonNull Task<AuthResult> task) {
+        //    if (task.isSuccessful()) {
+        //  Toast.makeText(getActivity(), "Salvo", Toast.LENGTH_SHORT).show();
+        // } else {
+        //  Toast.makeText(getActivity(), "Erro", Toast.LENGTH_SHORT).show();
+
+        // }
+        //  }
+        //  });
+
+        // }
+
+
+
+        // });
+
         botaocadastrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mFirebaseAuth.createUserWithEmailAndPassword(email.getText().toString(), senha.getText().toString())
-                        .addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                if (task.isSuccessful()) {
-                                    Toast.makeText(getActivity(), "Salvo", Toast.LENGTH_SHORT).show();
-                                } else {
-                                    Toast.makeText(getActivity(), "Erro", Toast.LENGTH_SHORT).show();
+                usuario.setNome(nome.getText().toString());
+                usuario.setSenha(senha.getText().toString());
+                usuario.setTelefone(telefone.getText().toString());
+                usuario.setEmail(email.getText().toString());
 
-                                }
-                            }
-                        });
 
+
+                ref.child(usuario.getNome()).setValue(usuario);
+
+
+    }
+
+
+
+});
+
+
+     botaovoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(getActivity().getApplication(), NavigacaoCActivity.class);
+                startActivity(intent);
             }
-
-
-
-        });
-
+     });
 
         return vista;
     }
